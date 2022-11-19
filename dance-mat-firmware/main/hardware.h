970df4ba643b9cf507c3af5ae506aedb3f84d5a7
Danny Piper <djpiper28@gmail.com>
2022-11-19 13:44:33 +0000

    feat: added LCD to compoents

---
+++ dance-mat-firmware/main/hardware.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define D_7 39
+#define D_6 34
+#define D_5 35
+#define D_4 32
+#define D_3 33
+#define D_2 25
+#define D_1 26
+#define D_0 27
+
+#define RS 14
+#define RW 12
+
+#define PS_DATA 17
+#define PS_COMMAND 16
+#define PS_ATT 4
+#define PS_CLOCK 2
+#define PS_ACK 15
