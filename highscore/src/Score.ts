2f3473c01773ac302812746b1679d1b77111b995
Leondro Lio <leondro.lio@iuk.ukri.org>
2022-11-19 18:47:52 +0000

    feat: Setup win uploads

--- highscore/src/Score.ts
+++ highscore/src/Score.ts
@@ -1,6 +1,9 @@
 interface IScore {
   score: number;
   name: string;
+  photograph: string;
+  image: string;
+  timestamp: number;
 }
 
 class Score {
--- highscore/src/index.ts
+++ highscore/src/index.ts
@@ -1,13 +1,40 @@
+import { Storage } from "@google-cloud/storage";
+import { ServiceAccountCredentials } from "google-spreadsheet";
+import { readFile, rm } from "node:fs/promises";
 import { Photographer } from "./Photographer";
 import { Spreadsheet } from "./Spreadsheet";
 
 const spreadsheet = new Spreadsheet();
 const photographer = new Photographer();
+const storage = new Storage({ keyFilename: "./serviceAccount.json" });
 
 const main = async () => {
-  await spreadsheet.init();
-  await spreadsheet.addScore({ name: "Danny", score: 100 });
-  await photographer.takePhoto();
+  const authFile = await readFile("./serviceAccount.json", {
+    encoding: "utf-8",
+  });
+  const authData = JSON.parse(authFile);
+
+  let file = await photographer.takePhoto();
+  console.log(file);
+
+  const newFilename = Date.now().toString(10) + ".jpg";
+
+  await storage.bucket("highscore-members").upload(file, {
+    destination: newFilename,
+  });
+
+  console.log(newFilename);
+
+  await rm(file);
+
+  await spreadsheet.init(authData as ServiceAccountCredentials);
+  await spreadsheet.addScore({
+    name: "Danny",
+    score: 100,
+    photograph: `https://storage.googleapis.com/highscore-members/${newFilename}`,
+    image: "=IMAGE(INDIRECT(ADDRESS(ROW(),COLUMN()-1)))",
+    timestamp: Date.now(),
+  });
 };
 
 main();
