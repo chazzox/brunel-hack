093e30d12151517ad4d1aeac36e4f26be4e332d3
chazzox <caylott@outlook.com>
2022-11-20 09:07:42 +0000

    feat: added gameEnd component

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
 	import Heart from './assets/heart.svelte';
-	import Logo from "./assets/logo/index.svelte";
+	import Logo from './assets/logo/index.svelte';
 
 	import Progress from './components/progress.svelte';
 	import classnames from 'classnames';
 	import { onMount } from 'svelte';
+	import GameOver from './components/gameOver.svelte';
 
 	let userIsCorrect = true;
 	let userText = '';
 
-	let hasGameEnded = false;
+	let hasGameEnded = !false;
 	let didBeginTyping = false;
 	// game state
 	let score = 0;
@@ -35,14 +36,13 @@
 		userIsCorrect = words.join(' ').startsWith(userText);
 		console.log(userIsCorrect, words, userText);
 	}
-  
+
 	const handleEnterPress = async () => {
 		if (!userIsCorrect) {
 			// animation shake
 			return;
 		}
 		if (progress <= 0) return;
-
 
 		// removing words
 		const userWords = userText.split(' ');
@@ -88,7 +88,7 @@
 			!userIsCorrect && 'neonIn'
 		)}
 	>
-  	<Logo />
+		<Logo />
 		<div class="mx-auto text-white/80"><p>user score: {score}</p></div>
 
 		<div class="flex flex-1 flex-col items-center justify-center gap-24">
@@ -121,9 +121,7 @@
 		>
 	</main>
 {:else}
-	<main class="flex min-h-screen flex-col items-center justify-center bg-black p-10 text-white">
-		oioi
-	</main>
+	<GameOver {score} />
 {/if}
 
 <style>
@@ -131,7 +129,7 @@
 		box-shadow: 0 0 2rem #dc2626, 0 0 0.8rem #dc2626, 0 0 2rem #dc2626;
 	}
 
-	.neonIn {
+	:global(.neonIn) {
 		box-shadow: 0 0 0.2rem #fff, 0 0 0.2rem #fff, 0 0 2rem #dc2626, 0 0 0.8rem #dc2626,
 			0 0 2.8rem #dc2626, inset 0 0 1.3rem #dc2626;
 	}
