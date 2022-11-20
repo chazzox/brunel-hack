80ca6a25e3668d9cbae2e21faf1b14c4d25aab68
Leo Durrant <poweredbykurisu@hotmail.com>
2022-11-20 02:54:13 +0000

    fixed: bullshit

--- dance-mat-firmware/main/main.c
+++ dance-mat-firmware/main/main.c
@@ -25,55 +25,57 @@ void device_hooks_fn(struct mg_connection *, int, void *, void *);
 
 void app_main(void)
 {
-    printf("Please wait, starting up...\n");
+        printf("Please wait, starting up...\n");
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    lprintf(LOG_INFO, "This is %s chip with %d CPU core(s), WiFi%s%s%s, ",
-            CONFIG_IDF_TARGET,
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "",
-            (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+        /* Print chip information */
+        esp_chip_info_t chip_info;
+        uint32_t flash_size;
+        esp_chip_info(&chip_info);
+        lprintf(LOG_INFO, "This is %s chip with %d CPU core(s), WiFi%s%s%s, ",
+                CONFIG_IDF_TARGET,
+                chip_info.cores,
+                (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+                (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "",
+                (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
 
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    lprintf(LOG_INFO, "silicon revision v%d.%d, ", major_rev, minor_rev);
-    if (esp_flash_get_size(NULL, &flash_size) != ESP_OK)
-    {
-            printf("Get flash size failed");
-            return;
-    }
+        unsigned major_rev = chip_info.revision / 100;
+        unsigned minor_rev = chip_info.revision % 100;
+        lprintf(LOG_INFO, "silicon revision v%d.%d, ", major_rev, minor_rev);
+        if (esp_flash_get_size(NULL, &flash_size) != ESP_OK)
+        {
+                printf("Get flash size failed");
+                return;
+        }
 
-    lprintf(LOG_INFO, "%uMB %s flash\n", flash_size / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+        lprintf(LOG_INFO, "%uMB %s flash\n", flash_size / (1024 * 1024),
+                (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    lprintf(LOG_INFO, "Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
+        lprintf(LOG_INFO, "Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
 
-    lprintf(LOG_INFO, "Setting up DDR mat...\n");
+        lprintf(LOG_INFO, "Setting up DDR mat...\n");
 
-    touch_pads_t output;
-    memset(&output, 0, sizeof(output));
+        touch_pads_t output;
+        memset(&output, 0, sizeof(output));
 
-    init_pads(&output);
+        init_pads(&output);
 
-    // Setup wifi. This function is implemented in wifi.c
-    // It blocks until connected to the configured WiFi network
-    lprintf(LOG_INFO, "Setting WiFi up...\n");
-    
-    void wifi_init(const char *ssid, const char *pass);
-    wifi_init(WIFI_SSID, WIFI_PASS);
+        // Setup wifi. This function is implemented in wifi.c
+        // It blocks until connected to the configured WiFi network
+        lprintf(LOG_INFO, "Setting WiFi up...\n");
 
-    // Connected to WiFi, now start HTTP server
-    struct mg_mgr mgr;
-    mg_log_set(MG_LL_DEBUG); // Set log level
-    mg_mgr_init(&mgr);
-    MG_INFO(("Mongoose v%s on %s", MG_VERSION, s_listening_url));
-    mg_http_listen(&mgr, s_listening_url, device_hooks_fn, &mgr);
-    
-    for (;; usleep(20))
-            mg_mgr_poll(&mgr, 1000); // Infinite event loop
-            print_pads(&output);
+        void wifi_init(const char *ssid, const char *pass);
+        wifi_init(WIFI_SSID, WIFI_PASS);
+
+        // Connected to WiFi, now start HTTP server
+        struct mg_mgr mgr;
+        mg_log_set(MG_LL_DEBUG); // Set log level
+        mg_mgr_init(&mgr);
+        MG_INFO(("Mongoose v%s on %s", MG_VERSION, s_listening_url));
+        mg_http_listen(&mgr, s_listening_url, device_hooks_fn, &mgr);
+
+        for (;; usleep(20))
+        {
+                mg_mgr_poll(&mgr, 1000); // Infinite event loop
+                print_pads(&output);
+        }
 }
