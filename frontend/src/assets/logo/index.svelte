85248e16419c616c3932ad27cbea663a3a1f74c2
Leondro Lio <leondro.lio@iuk.ukri.org>
2022-11-20 06:44:54 +0000

    feat: add white arrow

--- frontend/src/assets/logo/index.svelte
+++ frontend/src/assets/logo/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import imageArrow from "./white_arrow.png";
   import imageH from "./H.png"
   import imageHa from "./Ha.png"
   import imageHac from "./Hac.png"
@@ -20,27 +21,44 @@
 </script>
 
 <style lang="scss">
+  $delayGap: 0.05s;
+  $scaleFactor: 4;
+
+  .container {
+    position: fixed;
+  }
+
   .logo {
-    padding-top: 5em;
+    padding-top: 4em;
     display: flex;
     flex-direction: column;
-    gap: 5em;
+    position: relative;
   }
 
   .line1, .line2 {
     // Top space space
     display: flex;
-    gap: 3em;
+    gap: 2em;
+    padding-left: 10em;
   }
 
   .line2 {
-    margin-left: 2em;
+    margin-left: 1em;
   }
 
   .hack1, .hack2, .revolutionLine {
     // Character spacing
     display: flex;
-    gap: 2em;
+  }
+
+  .divArrow {
+    position: absolute;
+    top: 50px;
+    left: 0;
+  }
+
+  .imageArrow {
+    height: 200px;
   }
   
   .divH,
@@ -61,8 +79,8 @@
   .divHackHackRevoluti,
   .divHackHackRevolutio,
   .divHackHackRevolution {
-    display: grid;
-    align-items: baseline;
+    display: flex;
+    align-items: flex-end;
     animation-name: move;
     animation-duration: 2s;
     animation-iteration-count: infinite;
@@ -71,29 +89,24 @@
     image-rendering: crisp-edges;
   }
 
-  .imageH,
-  .imageHa,
-  .imageHac,
-  .imageHack,
-  .imageHackH,
-  .imageHackHa,
-  .imageHackHac,
-  .imageHackHack,
-  .imageHackHackR,
-  .imageHackHackRe,
-  .imageHackHackRev,
-  .imageHackHackRevo,
-  .imageHackHackRevol,
-  .imageHackHackRevolu,
-  .imageHackHackRevolut,
-  .imageHackHackRevoluti,
-  .imageHackHackRevolutio,
-  .imageHackHackRevolution {
-    transform-origin: bottom left;
-    transform: scale(4);
-  }
-
-  $delayGap: 0.05s;
+  .imageH { width: 12px * $scaleFactor }
+  .imageHa { width: 9px * $scaleFactor }
+  .imageHac { width: 9px * $scaleFactor }
+  .imageHack { width: 9px * $scaleFactor }
+  .imageHackH { width: 12px * $scaleFactor }
+  .imageHackHa { width: 9px * $scaleFactor }
+  .imageHackHac { width: 9px * $scaleFactor }
+  .imageHackHack { width: 9px * $scaleFactor }
+  .imageHackHackR { width: 16px * $scaleFactor }
+  .imageHackHackRe { width: 13px * $scaleFactor }
+  .imageHackHackRev { width: 13px * $scaleFactor }
+  .imageHackHackRevo { width: 13px * $scaleFactor }
+  .imageHackHackRevol { width: 5px * $scaleFactor }
+  .imageHackHackRevolu { width: 12px * $scaleFactor }
+  .imageHackHackRevolut { width: 13px * $scaleFactor }
+  .imageHackHackRevoluti { width: 5px * $scaleFactor }
+  .imageHackHackRevolutio { width: 13px * $scaleFactor }
+  .imageHackHackRevolution { width: 13px * $scaleFactor }
 
   .divH {animation-delay: $delayGap * 1;}
   .divHa {animation-delay: $delayGap * 2;}
@@ -127,33 +140,38 @@
   }
 </style>
 
-<div class="logo">
-  <div class="line1">
-    <div class="hack1">
-      <div class="divH"><img class="imageH" src="{imageH}" /></div>
-      <div class="divHa"><img class="imageHa" src="{imageHa}" /></div>
-      <div class="divHac"><img class="imageHac" src="{imageHac}" /></div>
-      <div class="divHack"><img class="imageHack" src="{imageHack}" /></div>
+<div class="container">
+  <div class="logo">
+    <div class="divArrow">
+      <img class="imageArrow" src="{imageArrow}" />
     </div>
-    <div class="hack2">
-      <div class="divHackH"><img class="imageHackH" src="{imageHackH}" /></div>
-      <div class="divHackHa"><img class="imageHackHa" src="{imageHackHa}" /></div>
-      <div class="divHackHac"><img class="imageHackHac" src="{imageHackHac}" /></div>
-      <div class="divHackHack"><img class="imageHackHack" src="{imageHackHack}" /></div>
+    <div class="line1">
+      <div class="hack1">
+        <div class="divH"><img class="imageH" src="{imageH}" /></div>
+        <div class="divHa"><img class="imageHa" src="{imageHa}" /></div>
+        <div class="divHac"><img class="imageHac" src="{imageHac}" /></div>
+        <div class="divHack"><img class="imageHack" src="{imageHack}" /></div>
+      </div>
+      <div class="hack2">
+        <div class="divHackH"><img class="imageHackH" src="{imageHackH}" /></div>
+        <div class="divHackHa"><img class="imageHackHa" src="{imageHackHa}" /></div>
+        <div class="divHackHac"><img class="imageHackHac" src="{imageHackHac}" /></div>
+        <div class="divHackHack"><img class="imageHackHack" src="{imageHackHack}" /></div>
+      </div>
     </div>
-  </div>
-  <div class="line2">
-    <div class="revolutionLine">
-      <div class="divHackHackR"><img class="imageHackHackR" src="{imageHackHackR}" /></div>
-      <div class="divHackHackRe"><img class="imageHackHackRe" src="{imageHackHackRe}" /></div>
-      <div class="divHackHackRev"><img class="imageHackHackRev" src="{imageHackHackRev}" /></div>
-      <div class="divHackHackRevo"><img class="imageHackHackRevo" src="{imageHackHackRevo}" /></div>
-      <div class="divHackHackRevol"><img class="imageHackHackRevol" src="{imageHackHackRevol}" /></div>
-      <div class="divHackHackRevolu"><img class="imageHackHackRevolu" src="{imageHackHackRevolu}" /></div>
-      <div class="divHackHackRevolut"><img class="imageHackHackRevolut" src="{imageHackHackRevolut}" /></div>
-      <div class="divHackHackRevoluti"><img class="imageHackHackRevoluti" src="{imageHackHackRevoluti}" /></div>
-      <div class="divHackHackRevolutio"><img class="imageHackHackRevolutio" src="{imageHackHackRevolutio}" /></div>
-      <div class="divHackHackRevolution"><img class="imageHackHackRevolution" src="{imageHackHackRevolution}" /></div>
+    <div class="line2">
+      <div class="revolutionLine">
+        <div class="divHackHackR"><img class="imageHackHackR" src="{imageHackHackR}" /></div>
+        <div class="divHackHackRe"><img class="imageHackHackRe" src="{imageHackHackRe}" /></div>
+        <div class="divHackHackRev"><img class="imageHackHackRev" src="{imageHackHackRev}" /></div>
+        <div class="divHackHackRevo"><img class="imageHackHackRevo" src="{imageHackHackRevo}" /></div>
+        <div class="divHackHackRevol"><img class="imageHackHackRevol" src="{imageHackHackRevol}" /></div>
+        <div class="divHackHackRevolu"><img class="imageHackHackRevolu" src="{imageHackHackRevolu}" /></div>
+        <div class="divHackHackRevolut"><img class="imageHackHackRevolut" src="{imageHackHackRevolut}" /></div>
+        <div class="divHackHackRevoluti"><img class="imageHackHackRevoluti" src="{imageHackHackRevoluti}" /></div>
+        <div class="divHackHackRevolutio"><img class="imageHackHackRevolutio" src="{imageHackHackRevolutio}" /></div>
+        <div class="divHackHackRevolution"><img class="imageHackHackRevolution" src="{imageHackHackRevolution}" /></div>
+      </div>
     </div>
   </div>
 </div>
